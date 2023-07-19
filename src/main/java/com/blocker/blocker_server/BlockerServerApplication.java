9c04d60f5fa6d7c931101e4e6a8e705b515cc3d3
preferrrr <zzangsh2946@naver.com>
2023-07-19 19:38:44 +0900

    first commit

---
+++ src/main/java/com/blocker/blocker_server/BlockerServerApplication.java
@@ -0,0 +1,13 @@
+package com.blocker.blocker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blocker/blocker_server/BlockerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocker.blocker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
