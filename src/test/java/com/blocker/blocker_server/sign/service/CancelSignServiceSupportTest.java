d738805790f7f5ba84b5aa9c687873269521a7cb
preferrrr <zzangsh2946@naver.com>
2024-01-11 20:59:35 +0900

    feat : CancelSign 테스크 코드 추가

---
+++ src/test/java/com/blocker/blocker_server/sign/service/CancelSignServiceSupportTest.java
@@ -0,0 +1,275 @@
+package com.blocker.blocker_server.sign.service;
+
+import com.blocker.blocker_server.IntegrationTestSupport;
+import com.blocker.blocker_server.contract.domain.CancelContract;
+import com.blocker.blocker_server.contract.domain.CancelContractState;
+import com.blocker.blocker_server.contract.domain.Contract;
+import com.blocker.blocker_server.contract.repository.CancelContractRepository;
+import com.blocker.blocker_server.contract.repository.ContractRepository;
+import com.blocker.blocker_server.sign.domain.AgreementSign;
+import com.blocker.blocker_server.sign.domain.CancelSign;
+import com.blocker.blocker_server.sign.exception.*;
+import com.blocker.blocker_server.sign.repository.AgreementSignRepository;
+import com.blocker.blocker_server.sign.repository.CancelSignRepository;
+import com.blocker.blocker_server.user.domain.User;
+import com.blocker.blocker_server.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class CancelSignServiceSupportTest extends IntegrationTestSupport {
+
+
+    @Autowired
+    private CancelSignServiceSupport cancelSignServiceSupport;
+    @Autowired
+    private AgreementSignRepository agreementSignRepository;
+    @Autowired
+    private CancelContractRepository cancelContractRepository;
+    @Autowired
+    private CancelSignRepository cancelSignRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ContractRepository contractRepository;
+
+    @AfterEach
+    void tearDown() {
+        cancelSignRepository.deleteAllInBatch();
+        cancelContractRepository.deleteAllInBatch();
+        agreementSignRepository.deleteAllInBatch();
+        contractRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("체결된 계약서가 아니라면 IsNotConcludeContractForCancelException을 던진다.")
+    @Test
+    void checkIsConcludeContract() {
+
+        /** given */
+
+        User user = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        userRepository.save(user);
+
+        Contract NOT_PROCEED_contract = Contract.create(user, "testTitle", "testContent");
+        Contract PROCEED_contract = Contract.create(user, "testTitle", "testContent");
+        PROCEED_contract.updateStateToProceed();
+        contractRepository.saveAll(List.of(NOT_PROCEED_contract, PROCEED_contract));
+
+        /** when then */
+
+        assertThatThrownBy(() -> cancelSignServiceSupport.checkIsConcludeContract(NOT_PROCEED_contract))
+                .isInstanceOf(IsNotConcludeContractForCancelException.class);
+        assertThatThrownBy(() -> cancelSignServiceSupport.checkIsConcludeContract(PROCEED_contract))
+                .isInstanceOf(IsNotConcludeContractForCancelException.class);
+
+    }
+
+    @DisplayName("계약 파기 요청에서 계약서의 참여자가 아니면 IsNotContractParticipantForCancelException을 던진다.")
+    @Test
+    void checkIsParticipantForCancel() {
+
+        /** given */
+        User participant1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User participant2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User notParticipant = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+        userRepository.saveAll(List.of(participant1, participant2, notParticipant));
+
+        Contract contract = Contract.create(participant1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        AgreementSign agreementSign1 = AgreementSign.create(participant1, contract);
+        AgreementSign agreementSign2 = AgreementSign.create(participant2, contract);
+        agreementSignRepository.saveAll(List.of(agreementSign1, agreementSign2));
+
+
+        /** when then */
+
+        assertThatThrownBy(() -> cancelSignServiceSupport.checkIsParticipantForCancel(notParticipant, contract))
+                .isInstanceOf(IsNotContractParticipantForCancelException.class);
+
+    }
+
+    @DisplayName("CancelContract와 AgreementSign 리스트로 CancelSign 리스트를 만든다.")
+    @Test
+    void createCancelSigns() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User user3 = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        Contract contract = Contract.create(user1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        AgreementSign agreementSign1 = AgreementSign.create(user1, contract);
+        AgreementSign agreementSign2 = AgreementSign.create(user2, contract);
+        AgreementSign agreementSign3 = AgreementSign.create(user3, contract);
+        agreementSignRepository.saveAll(List.of(agreementSign1, agreementSign2, agreementSign3));
+
+        CancelContract cancelContract = CancelContract.create(user1, contract, "testTitle", "testContent");
+        cancelContractRepository.save(cancelContract);
+
+        /** when */
+
+        List<CancelSign> result = cancelSignServiceSupport.createCancelSigns(cancelContract, List.of(agreementSign1, agreementSign2, agreementSign3));
+
+        /** then */
+
+        assertThat(result).hasSize(3);
+
+        List<String> participants = result.stream()
+                .map(cancelSign -> cancelSign.getUser().getEmail())
+                .collect(Collectors.toList());
+        assertThat(participants.contains(user1.getEmail()));
+        assertThat(participants.contains(user2.getEmail()));
+        assertThat(participants.contains(user3.getEmail()));
+
+        assertThat(result.get(0).getCancelContract().getCancelContractId()).isEqualTo(cancelContract.getCancelContractId());
+
+    }
+
+    @DisplayName("파기 계약서를 저장한다.")
+    @Test
+    void saveCancelContract() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        userRepository.save(user1);
+
+        Contract contract = Contract.create(user1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        CancelContract cancelContract = CancelContract.create(user1, contract, "testTitle", "testContent");
+
+        /** when */
+
+        cancelSignServiceSupport.saveCancelContract(cancelContract);
+
+        /** then */
+
+        List<CancelContract> cancelContracts = cancelContractRepository.findAll();
+        assertThat(cancelContracts).hasSize(1);
+
+    }
+
+    @DisplayName("CancelSign 리스트를 저장한다.")
+    @Test
+    void saveCancelSigns() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User user3 = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        Contract contract = Contract.create(user1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        CancelContract cancelContract = CancelContract.create(user1, contract, "testTitle", "testContent");
+        cancelContractRepository.save(cancelContract);
+
+        CancelSign cancelSign1 = CancelSign.create(user1, cancelContract);
+        CancelSign cancelSign2 = CancelSign.create(user2, cancelContract);
+        CancelSign cancelSign3 = CancelSign.create(user3, cancelContract);
+
+        /** when */
+
+        cancelSignServiceSupport.saveCancelSigns(List.of(cancelSign1, cancelSign2, cancelSign3));
+
+        /** then */
+
+        List<CancelSign> cancelSigns = cancelSignRepository.findAll();
+        assertThat(cancelSigns).hasSize(3);
+        assertThat(cancelSigns.get(0).getCancelContract().getCancelContractId()).isEqualTo(cancelContract.getCancelContractId());
+
+        List<String> participants = cancelSigns.stream()
+                .map(cancelSign -> cancelSign.getUser().getEmail())
+                .collect(Collectors.toList());
+        assertThat(participants.contains(user1.getEmail()));
+        assertThat(participants.contains(user2.getEmail()));
+        assertThat(participants.contains(user3.getEmail()));
+
+    }
+
+    @DisplayName("CancelSign 리스트 중 나의 CancelSign을 반환한다.")
+    @Test
+    void getMyCancelSign() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User user3 = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        Contract contract = Contract.create(user1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        CancelContract cancelContract = CancelContract.create(user1, contract, "testTitle", "testContent");
+        cancelContractRepository.save(cancelContract);
+
+        CancelSign cancelSign1 = CancelSign.create(user1, cancelContract);
+        CancelSign cancelSign2 = CancelSign.create(user2, cancelContract);
+        CancelSign cancelSign3 = CancelSign.create(user3, cancelContract);
+        cancelSignRepository.saveAll(List.of(cancelSign1, cancelSign2, cancelSign3));
+
+        /** when */
+
+        CancelSign myCancelSign = getMyCancelSign(user1.getEmail(), List.of(cancelSign1, cancelSign2, cancelSign3));
+
+        /** then */
+
+        assertThat(myCancelSign.getUser().getEmail()).isEqualTo(user1.getEmail());
+
+    }
+    public CancelSign getMyCancelSign(String email, List<CancelSign> cancelSigns) {
+        return cancelSigns.stream()
+                .filter(cancelSign -> cancelSign.getUser().getEmail().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new IsNotCancelContractParticipantException("email: " + email));
+    }
+
+    @DisplayName("CancelSign들의 SignState가 모두 Y이면, CancelContract의 CancelContractState를 CANCELED로 바꾼다.")
+    @Test
+    void checkIsAllAgree() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User user3 = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        Contract contract = Contract.create(user1, "testTitle", "testContent");
+        contractRepository.save(contract);
+
+        CancelContract cancelContract = CancelContract.create(user1, contract, "testTitle", "testContent");
+        cancelContractRepository.save(cancelContract);
+
+        CancelSign cancelSign1 = CancelSign.create(user1, cancelContract);
+        cancelSign1.sign();
+        CancelSign cancelSign2 = CancelSign.create(user2, cancelContract);
+        cancelSign2.sign();
+        CancelSign cancelSign3 = CancelSign.create(user3, cancelContract);
+        cancelSign3.sign();
+        cancelSignRepository.saveAll(List.of(cancelSign1, cancelSign2, cancelSign3));
+
+        cancelContract = cancelContractRepository.findCancelContractWithSignsById(cancelContract.getCancelContractId()).get();
+
+        /** when */
+
+        cancelSignServiceSupport.checkIsAllAgree(cancelContract);
+
+        /** then */
+
+        assertThat(cancelContract.getCancelContractState()).isEqualTo(CancelContractState.CANCELED);
+    }
+
+}
--- src/test/java/com/blocker/blocker_server/sign/service/CancelSignServiceTest.java
+++ src/test/java/com/blocker/blocker_server/sign/service/CancelSignServiceTest.java
@@ -7,6 +7,8 @@ import com.blocker.blocker_server.contract.domain.Contract;
 import com.blocker.blocker_server.contract.domain.ContractState;
 import com.blocker.blocker_server.contract.repository.CancelContractRepository;
 import com.blocker.blocker_server.contract.repository.ContractRepository;
+import com.blocker.blocker_server.contract.service.CancelContractServiceSupport;
+import com.blocker.blocker_server.contract.service.ContractServiceSupport;
 import com.blocker.blocker_server.sign.domain.AgreementSign;
 import com.blocker.blocker_server.sign.domain.CancelSign;
 import com.blocker.blocker_server.sign.domain.SignState;
@@ -14,9 +16,13 @@ import com.blocker.blocker_server.sign.repository.AgreementSignRepository;
 import com.blocker.blocker_server.sign.repository.CancelSignRepository;
 import com.blocker.blocker_server.user.domain.User;
 import com.blocker.blocker_server.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,12 +31,90 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CancelSignServiceTest {
 
+    @InjectMocks
+    private CancelSignService cancelSignService;
+    @Mock
+    private CancelSignServiceSupport cancelSignServiceSupport;
+    @Mock
+    private ContractServiceSupport contractServiceSupport;
+    @Mock
+    private CancelContractServiceSupport cancelContractServiceSupport;
 
+    private User user;
+    private Contract contract;
+    private CancelContract cancelContract;
+    private CancelSign cancelSign;
+
+    @BeforeEach
+    void setUp() {
+        user = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        contract = Contract.create(user, "testTitle", "testContent");
+        cancelContract = CancelContract.create(user, contract, "testTitle", "testContent");
+        cancelSign = CancelSign.create(user, cancelContract);
+    }
+
+    @DisplayName("계약을 파기하면 CancelContact와 파기 계약 참여자들의 CancelSigns가 저장된다.")
+    @Test
+    void cancelContract() {
+
+        /** given */
+
+        given(contractServiceSupport.getContractWIthSignsById(anyLong())).willReturn(contract);
+        willDoNothing().given(cancelSignServiceSupport).checkIsConcludeContract(any(Contract.class));
+        willDoNothing().given(cancelSignServiceSupport).checkIsParticipantForCancel(any(User.class), any(Contract.class));
+        willDoNothing().given(cancelSignServiceSupport).checkIsCancelingContract(any(Contract.class));
+        given(cancelSignServiceSupport.createCancelSigns(any(CancelContract.class), anyList())).willReturn(mock(List.class));
+        willDoNothing().given(cancelSignServiceSupport).saveCancelContract(any(CancelContract.class));
+        willDoNothing().given(cancelSignServiceSupport).saveCancelSigns(anyList());
+
+        /** when */
+
+        cancelSignService.cancelContract(user, 1l);
+
+        /** then */
+
+        verify(contractServiceSupport, times(1)).getContractWIthSignsById(anyLong());
+        verify(cancelSignServiceSupport, times(1)).checkIsConcludeContract(any(Contract.class));
+        verify(cancelSignServiceSupport, times(1)).checkIsParticipantForCancel(any(User.class), any(Contract.class));
+        verify(cancelSignServiceSupport, times(1)).checkIsCancelingContract(any(Contract.class));
+        verify(cancelSignServiceSupport, times(1)).createCancelSigns(any(CancelContract.class), anyList());
+        verify(cancelSignServiceSupport, times(1)).saveCancelContract(any(CancelContract.class));
+        verify(cancelSignServiceSupport, times(1)).saveCancelSigns(anyList());
+
+
+    }
+
+    @DisplayName("파기 계약에 서명하면 나의 CancelSign의 State가 Y로 바뀐다.")
+    @Test
+    void signCancelContract() {
+
+        /** given */
+        given(cancelContractServiceSupport.getCancelContractWithSignsById(anyLong())).willReturn(cancelContract);
+        given(cancelSignServiceSupport.getMyCancelSign(anyString(), anyList())).willReturn(cancelSign);
+        willDoNothing().given(cancelSignServiceSupport).checkMySignStateIsN(any(CancelSign.class));
+        willDoNothing().given(cancelSignServiceSupport).checkIsAllAgree(any(CancelContract.class));
+
+        /** when */
+
+        cancelSignService.signCancelContract(user, 1l);
+
+        /** then */
+
+        verify(cancelContractServiceSupport, times(1)).getCancelContractWithSignsById(anyLong());
+        verify(cancelSignServiceSupport, times(1)).getMyCancelSign(anyString(), anyList());
+        verify(cancelSignServiceSupport, times(1)).checkMySignStateIsN(any(CancelSign.class));
+        verify(cancelSignServiceSupport, times(1)).checkIsAllAgree(any(CancelContract.class));
+
+        assertThat(cancelSign.getSignState()).isEqualTo(SignState.Y);
+
+    }
 
 }
