d644cc0fcd0d4d2f5be52451c4c0335fe8f36c8f
preferrrr <zzangsh2946@naver.com>
2024-01-12 20:10:23 +0900

    refactor : 채팅 Business와 Service 분리, Exception 분리, 필요하지 않은 Exception Class 삭제

--- src/main/java/com/blocker/blocker_server/commons/exception/GlobalExceptionHandler.java
+++ src/main/java/com/blocker/blocker_server/commons/exception/GlobalExceptionHandler.java
@@ -20,26 +20,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<?> handleExistUsernameException(final NotFoundException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND); /**404, db에 없음.*/
-    }
-
-    @ExceptionHandler({FailSaveSignatureException.class})
-    public ResponseEntity<?> handleFailSaveSignatureException(final FailSaveSignatureException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); /**500, 전자 서명 저장 실패.*/
-    }
-
-
-
     @ExceptionHandler({InvalidRefreshTokenException.class})
     public ResponseEntity<?> handleInvalidRefreshTokenException(final InvalidRefreshTokenException e) {
 
@@ -47,16 +27,6 @@ public class GlobalExceptionHandler {
         log.error(msg);
 
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); /**401, 유효하지 않은 리프레스 토큰.*/
-    }
-
-
-    @ExceptionHandler({ExistsSignatureException.class})
-    public ResponseEntity<?> handleExistsSignatureException(final ExistsSignatureException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 등록한 전자서명이 이미 존재.*/
     }
 
     @ExceptionHandler({ForbiddenException.class})
@@ -86,15 +56,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 이미 북마크로 등록했음.*/
     }
 
-    @ExceptionHandler({ModifyContractException.class})
-    public ResponseEntity<?> handleModifyContractException(final ModifyContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 진행 중이거나 완료된 계약은 수정 불가.*/
-    }
-
     @ExceptionHandler({InvalidQueryStringException.class})
     public ResponseEntity<?> handleInvalidQueryStringExceptionException(final InvalidQueryStringException e) {
 
@@ -104,113 +65,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 잘못된 쿼리스트링.*/
     }
 
-    @ExceptionHandler({ExistsAgreementSignException.class})
-    public ResponseEntity<?> handleInvalidQueryStringExceptionException(final ExistsAgreementSignException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 해당 계약서는 이미 서명 진행 중.*/
-    }
-
-
-
-    @ExceptionHandler({NotAllowModifyContractException.class})
-    public ResponseEntity<?> handleNotAllowedModifyContractException(final NotAllowModifyContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 체결 완료된 계약서는 수정할 수 없음.*/
-    }
-
-    @ExceptionHandler({NotAllowDeleteContractException.class})
-    public ResponseEntity<?> handleNotAllowDeleteContractException(final NotAllowDeleteContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 게시글 먼저 지워야함.*/
-    }
-
-    @ExceptionHandler({NotProceedContractException.class})
-    public ResponseEntity<?> handleNotProceedContractException(final NotProceedContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 진행 중 계약서가 아님.*/
-    }
-
-    @ExceptionHandler({DuplicateSignException.class})
-    public ResponseEntity<?> handleDuplicateSignException(final DuplicateSignException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 계약서에 이미 서명함.*/
-    }
-
-    @ExceptionHandler({DuplicateContractException.class})
-    public ResponseEntity<?> handleDuplicateContractException(final DuplicateContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 계약서로 이미 게시글 작성함.*/
-    }
-
-    @ExceptionHandler({IsNotProceedContractException.class})
-    public ResponseEntity<?> handleIsNotProceedContractException(final IsNotProceedContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 미체결 계약서가 아님.*/
-    }
-
-    @ExceptionHandler({IsProceedContractException.class})
-    public ResponseEntity<?> handleIsProceedContractException(final IsProceedContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 진행 중 계약서가 아님.*/
-    }
-
-    @ExceptionHandler({NotConcludeContractException.class})
-    public ResponseEntity<?> handleNotConcludeContractException(final NotConcludeContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 체결된 계약서가 아님.*/
-    }
-
-    @ExceptionHandler({ExistsCancelSignException.class})
-    public ResponseEntity<?> handleExistsCancelSignException(final ExistsCancelSignException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.CONFLICT); /**409, 이미 파기 계약 진행 중.*/
-    }
-
-    @ExceptionHandler({NotCancelingContractException.class})
-    public ResponseEntity<?> handleNotCancelingContractException(final NotCancelingContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 파기 진행 중 계약서가 아님.*/
-    }
-
-    @ExceptionHandler({NotCanceledContractException.class})
-    public ResponseEntity<?> handleNotCanceledContractException(final NotCanceledContractException e) {
-
-        String msg = e.getNAME() + ": [" + e.getMessage() + "]";
-        log.error(msg);
-
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); /**400, 파기 체결 계약서가 아님.*/
-    }
 }
