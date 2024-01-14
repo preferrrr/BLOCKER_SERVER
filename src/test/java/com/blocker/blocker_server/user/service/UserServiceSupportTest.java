258a5ab32e8f81df62370a3cba501affd442d9b5
preferrrr <zzangsh2946@naver.com>
2024-01-14 22:56:02 +0900

    feat : User 테스트 코드 추가

---
+++ src/test/java/com/blocker/blocker_server/user/service/UserServiceSupportTest.java
@@ -0,0 +1,185 @@
+package com.blocker.blocker_server.user.service;
+
+import com.blocker.blocker_server.IntegrationTestSupport;
+import com.blocker.blocker_server.commons.jwt.JwtProvider;
+import com.blocker.blocker_server.user.domain.User;
+import com.blocker.blocker_server.user.dto.response.SearchUserResponse;
+import com.blocker.blocker_server.user.exception.InvalidRefreshTokenException;
+import com.blocker.blocker_server.user.exception.UserNotFoundException;
+import com.blocker.blocker_server.user.repository.UserRepository;
+import org.apache.kafka.common.protocol.types.Field;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceSupportTest extends IntegrationTestSupport {
+
+    @Autowired
+    private UserServiceSupport userServiceSupport;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("email로 User를 찾는다.")
+    @Test
+    void getUserByEmail() {
+
+        /** given */
+
+        User user = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        userRepository.save(user);
+
+        /** when */
+
+        User result = userServiceSupport.getUserByEmail("testEmail");
+
+        /** then */
+
+        assertThat(result).isNotNull();
+
+    }
+
+    @DisplayName("email로 User를 찾는데 없으면 UserNotFoundException을 던진다.")
+    @Test
+    void getUserByEmailException() {
+
+        /** given */
+
+        /** when then */
+        assertThatThrownBy(() -> userServiceSupport.getUserByEmail("testEmail"))
+                .isInstanceOf(UserNotFoundException.class);
+
+
+    }
+
+    @DisplayName("email 또는 name에 keyword가 포함된 User를 찾는다.")
+    @Test
+    void searchUsers() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("222", "222222", "2222222222", "2222222", List.of("USER"));
+        User user3 = User.create("333", "testName3", "3333333333", "333333333", List.of("USER"));
+
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        /** when */
+
+        List<User> result = userServiceSupport.searchUsers("test");
+
+        /** then */
+
+        assertThat(result).hasSize(2);
+
+        List<String> emails = result.stream()
+                .map(user -> user.getEmail())
+                .collect(Collectors.toList());
+        assertThat(emails)
+                .contains("testEmail")
+                .contains("333");
+
+    }
+
+    @DisplayName("User 리스트를 SearchUserResponse 리스트로 만든다.")
+    @Test
+    void createSearchUserResponse() {
+
+        /** given */
+
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        User user2 = User.create("testEmail2", "testName2", "testPicture", "testValue2", List.of("USER"));
+        User user3 = User.create("testEmail3", "testName3", "testPicture", "testValue3", List.of("USER"));
+
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        /** when */
+
+        List<SearchUserResponse> result = userServiceSupport.createSearchUserResponse(List.of(user1, user2, user3));
+
+        /** then */
+
+        assertThat(result).hasSize(3);
+
+        List<String> emails = result.stream()
+                .map(user -> user.getEmail())
+                .collect(Collectors.toList());
+        assertThat(emails)
+                .contains(user1.getEmail())
+                .contains(user2.getEmail())
+                .contains(user3.getEmail());
+
+    }
+
+    @DisplayName("refresh token value로 User를 찾는다.")
+    @Test
+    void getUserByRefreshTokenValue() {
+
+        /** given */
+
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        userRepository.save(user1);
+
+        String token = jwtProvider.createRefreshToken("testValue");
+        /** when */
+
+        User result = userServiceSupport.getUserByRefreshTokenValue(token);
+
+        /** then */
+        assertThat(result.getEmail()).isEqualTo(user1.getEmail());
+
+    }
+
+    @DisplayName("")
+    @Test
+    void createHeadersWithJwt() {
+
+        /** given */
+        User user1 = User.create("testEmail", "testName", "testPicture", "testValue", List.of("USER"));
+        userRepository.save(user1);
+
+        /** when */
+
+        HttpHeaders result = userServiceSupport.createHeadersWithJwt(user1.getEmail(), user1.getName(), user1.getRefreshtokenValue(), user1.getRoles());
+
+        /** then */
+
+        assertThat(result.containsKey("Authorization")).isTrue();
+        assertThat(result.containsKey("COOKIE")).isTrue();
+
+    }
+
+    @DisplayName("random uuid로 랜덤키를 생성하고 '-'를 제거한다.")
+    @Test
+    void getNewRefreshTokenValue() {
+
+        /** given */
+
+
+        /** when */
+        String result = userServiceSupport.getNewRefreshTokenValue();
+
+        /** then */
+
+        assertThat(!result.contains("-")).isTrue();
+    }
+
+
+
+}
---
+++ src/test/java/com/blocker/blocker_server/user/service/UserServiceTest.java
@@ -0,0 +1,78 @@
+package com.blocker.blocker_server.user.service;
+
+import com.blocker.blocker_server.user.domain.User;
+import com.blocker.blocker_server.user.dto.response.SearchUserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserServiceSupport userServiceSupport;
+
+    private User user;
+    @BeforeEach
+    void setUp() {
+        user = User.create("testEmail","testName","testPicture","testValue",List.of("USER"));
+    }
+
+    @DisplayName("JWT를 재발급하고, 헤더에 실어서 반환한다.")
+    @Test
+    void reissueToken() {
+
+        /** given */
+        given(userServiceSupport.getRefreshTokenFromCookie(anyString())).willReturn("testValue");
+        given(userServiceSupport.getUserByRefreshTokenValue(anyString())).willReturn(user);
+        given(userServiceSupport.getNewRefreshTokenValue()).willReturn("newValue");
+        given(userServiceSupport.createHeadersWithJwt(anyString(),anyString(),anyString(),anyList())).willReturn(mock(HttpHeaders.class));
+
+        /** when */
+
+        HttpHeaders result = userService.reissueToken("cookie");
+
+        /** then */
+
+        verify(userServiceSupport, times(1)).getRefreshTokenFromCookie(anyString());
+        verify(userServiceSupport, times(1)).getUserByRefreshTokenValue(anyString());
+        verify(userServiceSupport, times(1)).createHeadersWithJwt(anyString(),anyString(),anyString(),anyList());
+
+        assertThat(user.getRefreshtokenValue()).isEqualTo("newValue");
+    }
+
+    @DisplayName("이메일 또는 닉네임에 keyword가 포함된 사용자를 검색해서 반환한다.")
+    @Test
+    void searchUsers() {
+
+        /** given */
+
+        given(userServiceSupport.searchUsers(anyString())).willReturn(mock(List.class));
+        given(userServiceSupport.createSearchUserResponse(anyList())).willReturn(mock(List.class));
+
+        /** when */
+
+        List<SearchUserResponse> result = userService.searchUsers("keyword");
+
+        /** then */
+
+        verify(userServiceSupport, times(1)).searchUsers(anyString());
+        verify(userServiceSupport, times(1)).createSearchUserResponse(anyList());
+
+
+    }
+
+}
