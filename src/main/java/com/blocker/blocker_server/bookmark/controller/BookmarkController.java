961501c074da5e06d7e0e7d7e2c54240932042a9
preferrrr <zzangsh2946@naver.com>
2024-03-15 22:12:02 +0900

    feat : Bookmark 응답 Custom Response로 변경

--- src/main/java/com/blocker/blocker_server/bookmark/controller/BookmarkController.java
+++ src/main/java/com/blocker/blocker_server/bookmark/controller/BookmarkController.java
@@ -3,6 +3,8 @@ package com.blocker.blocker_server.bookmark.controller;
 import com.blocker.blocker_server.bookmark.service.BookmarkService;
 import com.blocker.blocker_server.bookmark.dto.request.SaveBookmarkRequestDto;
 import com.blocker.blocker_server.board.dto.response.GetBoardListResponseDto;
+import com.blocker.blocker_server.commons.response.BaseResponse;
+import com.blocker.blocker_server.commons.response.ListResponse;
 import com.blocker.blocker_server.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -23,30 +25,29 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping("")
-    public ResponseEntity<HttpStatus> saveBookmark(@AuthenticationPrincipal User user,
-                                                   @RequestBody SaveBookmarkRequestDto requestDto) {
+    public ResponseEntity<BaseResponse> saveBookmark(@AuthenticationPrincipal User user,
+                                                     @RequestBody SaveBookmarkRequestDto requestDto) {
 
         bookmarkService.saveBookmark(user, requestDto);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok(BaseResponse.ok());
 
     }
 
     @DeleteMapping("/{boardId}")
-    public ResponseEntity<HttpStatus> deleteBookmark(@AuthenticationPrincipal User user,
+    public ResponseEntity<BaseResponse> deleteBookmark(@AuthenticationPrincipal User user,
                                                      @PathVariable("boardId") Long boardId) {
         bookmarkService.deleteBookmark(user, boardId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok(BaseResponse.ok());
     }
 
     @GetMapping("/boards")
-    public ResponseEntity<List<GetBoardListResponseDto>> getBookmarkBoards(@AuthenticationPrincipal User user,
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<ListResponse<GetBoardListResponseDto>> getBookmarkBoards(@AuthenticationPrincipal User user,
+                                                                                   @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 
-        return new ResponseEntity<>(
-                bookmarkService.getBookmarkBoards(user, pageable),
-                HttpStatus.OK
+        return ResponseEntity.ok(
+                ListResponse.ok(bookmarkService.getBookmarkBoards(user, pageable))
         );
     }
 }
