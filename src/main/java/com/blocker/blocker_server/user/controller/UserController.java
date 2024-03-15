a2e54c0615ffc36666e184a537cb315ee979a69e
preferrrr <zzangsh2946@naver.com>
2024-03-15 20:12:06 +0900

    refactor : User request dto @Valid로 유효성 검사

--- src/main/java/com/blocker/blocker_server/user/controller/UserController.java
+++ src/main/java/com/blocker/blocker_server/user/controller/UserController.java
@@ -21,30 +21,25 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<HttpHeaders> login(@RequestBody LoginRequestDto requestDto) {
 
-        requestDto.validateFieldsNotNull();
-
-        ResponseEntity response = userService.login(requestDto);
-
-        return response;
+        return userService.login(requestDto);
     }
 
     @GetMapping("/reissue-token")
     public ResponseEntity<HttpHeaders> reissueToken(@RequestHeader("Cookie") String cookie) {
 
-        if(cookie.isEmpty() || cookie.isBlank())
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-
-        HttpHeaders headers = userService.reissueToken(cookie);
-
-        return new ResponseEntity<>(headers, HttpStatus.OK);
+        return new ResponseEntity<>(
+                userService.reissueToken(cookie),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<SearchUserResponse>> searchUsers(@RequestParam("keyword") String keyword) {
 
-        List<SearchUserResponse> result = userService.searchUsers(keyword);
-
-        return new ResponseEntity(result, HttpStatus.OK);
+        return new ResponseEntity(
+                userService.searchUsers(keyword),
+                HttpStatus.OK
+        );
     }
 
 }
