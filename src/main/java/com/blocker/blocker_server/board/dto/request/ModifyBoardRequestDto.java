8c00404de444d5d988ccbf1342a433c7503823aa
preferrrr <zzangsh2946@naver.com>
2024-04-16 16:45:19 +0900

    fix : ModifyBoardRequestDto Long 타입 유효성 검사 @NotNull로 변경

--- src/main/java/com/blocker/blocker_server/board/dto/request/ModifyBoardRequestDto.java
+++ src/main/java/com/blocker/blocker_server/board/dto/request/ModifyBoardRequestDto.java
@@ -1,6 +1,7 @@
 package com.blocker.blocker_server.board.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,7 @@ public class ModifyBoardRequestDto {
     private String content;
     private String info;
     private String representImage; // 대표이미지
-    @NotBlank(message = "계약서 인덱스는 null 또는 공백일 수 없습니다.")
+    @NotNull(message = "계약서 인덱스는 null 또는 공백일 수 없습니다.")
     private Long contractId;
     private List<Long> deleteImageIds;
     private List<String> addImageAddresses;
